4f31ba8cb0244eff8698c724cdb2735b4c464361
Manan Joshi <joshimanan265@gmail.com>
2024-07-08 11:23:58 +0530

    Add files via upload

---
+++ rectPerimeter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//perimeter of rectangle
+int main(){
+int a,b;
+printf("enter a");
+scanf("%d", &a);
+
+printf("enter b");
+scanf("%d", &b);
+
+printf("perimeter is : %d\n", 2*(a + b));
+return 0;
+}
