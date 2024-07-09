b9d1866eba767a54c6ff5e9ad0b8c31d14f03dc9
Manan Joshi <joshimanan265@gmail.com>
2024-07-09 11:52:31 +0530

    Add files via upload

---
+++ squarePattern.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+//program to print a 5*5 square pattern using NESTED LOOPS.
+	
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
